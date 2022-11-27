bd0de01320d905127eab6ed30d9c0963253f065b
Zaria7 <ruta.blozyte@gmail.com>
2022-11-27 15:14:13 +0200

    Uzbaigta istaigos repozitorija

--- src/main/java/com/example/BaigiamasisDarbas/controllers/IstaiguController.java
+++ src/main/java/com/example/BaigiamasisDarbas/controllers/IstaiguController.java
@@ -3,10 +3,7 @@ package com.example.BaigiamasisDarbas.controllers;
 import com.example.BaigiamasisDarbas.dao.istaigos.IstaiguReopzitorija;
 import com.example.BaigiamasisDarbas.dto.SkiepijimoIstaiga;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -26,5 +23,31 @@ public class IstaiguController {
     }
 
     @GetMapping("/api/istaigos")
-    public List<SkiepijimoIstaiga> gautiIstaigas() { return istaiguReopzitorija.gautiVisasSkiepijimoIstaigas();}
+    public List<SkiepijimoIstaiga> gautiIstaigas() {
+        return istaiguReopzitorija.gautiVisasSkiepijimoIstaigas();
+    }
+
+    @GetMapping("/api/istaigos/{id}")
+    public Optional<SkiepijimoIstaiga> gautiIstaigaPagalId(@PathVariable Long id) {
+        return istaiguReopzitorija.gautiSkiepijimoIstaigaPagalId(id);
+    }
+
+    @DeleteMapping("/api/istaigos")
+    public void istrintiIstaiga(@RequestParam Long id) {
+        istaiguReopzitorija.istringiSkiepijimoIstaiga(id);
+    }
+
+    @PutMapping("/api/istaigos/{id}")
+    public Optional<SkiepijimoIstaiga> atnaujintiIstaiga(
+            @RequestParam Long id,
+            @RequestBody SkiepijimoIstaiga naujaIstaigosInfo
+    ) {
+        var naujinamasIrasas = istaiguReopzitorija.gautiSkiepijimoIstaigaPagalId(id)
+                .orElseThrow(() -> new RuntimeException("Nera tokios imones su id: " + id));
+        naujinamasIrasas.setId(id);
+        naujinamasIrasas.setItaigosPavadinimas(naujaIstaigosInfo.getItaigosPavadinimas());
+        naujinamasIrasas.setMiestas(naujaIstaigosInfo.getMiestas());
+        naujinamasIrasas.setGydytojas(naujaIstaigosInfo.getGydytojas());
+        return istaiguReopzitorija.gautiSkiepijimoIstaigaPagalId(id);
+    }
 }
